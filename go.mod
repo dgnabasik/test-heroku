c64bdb10f3f9e26fbb765cebdf4bb3a9a4bea2aa
dgnabasik <DavidGnabasik@comcast.net>
2020-11-24 10:34:10 -0700

    Release 1.0.1

--- go.mod
+++ go.mod
@@ -3,13 +3,8 @@ module server
 go 1.15
 
 require (
-	github.com/andybalholm/brotli v1.0.1 // indirect
-	github.com/gofiber/cors v0.2.2
-	github.com/gofiber/fiber v1.13.3
-	github.com/gofiber/fiber/v2 v2.2.0
-	github.com/gofiber/utils v0.1.2 // indirect
-	github.com/gorilla/schema v1.2.0 // indirect
-	github.com/klauspost/compress v1.11.3 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
+	github.com/gin-gonic/gin v1.6.3
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -1,28 +1,67 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"net/http"
 	"os"
 
-	//"github.com/gofiber/cors"
-	"github.com/gofiber/fiber"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
 )
 
-func serveStatic(app *fiber.App) {
-	app.Static("/", "./build")
+// ErrorHandler func
+func ErrorHandler(ctx *gin.Context) {
+	ctx.Next()
+
+	if len(ctx.Errors) > 0 {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"errors": ctx.Errors,
+		})
+	}
 }
 
-func main() {
-	app := fiber.New()
-	//app.Use(cors.New())
-
-	serveStatic(app)
-
+// GetPort func:
+func GetPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "5000"
-
 	}
-	log.Printf("Listening on port %s\n\n", port)
-	app.Listen(port)
+	return port
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode) // Switch to "release" mode in production; or export GIN_MODE=release
+	router := gin.Default()
+	router.Use(cors.New(cors.Config{ // assume this config is overwritten by ContextOptions()?
+		AllowOrigins: []string{"*"}, // hostName
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		//AllowHeaders: []string{"X-CSRF-Token", "Authorization"},
+		AllowHeaders:     []string{"User-Agent", "Referrer", "Host", "Token", "Accept", "Content-Type", "Origin", "Content-Length", "X-Requested-With", "Accept-Encoding"},
+		AllowCredentials: true,
+		AllowAllOrigins:  false,
+		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+		AllowOriginFunc: func(origin string) bool {
+			return true // origin == hostName
+		},
+		MaxAge: 86400,
+	}))
+
+	router.Use(ErrorHandler)
+	// Serve frontend static files from /build when AllowDirectoryIndex=false. Same folder as in golang/Dockerfile: COPY --from=builder /build/ /app/
+	router.Use(static.Serve("/", static.LocalFile("./build", false)))
+
+	dataToUIPage := gin.H{
+		"REACT_APP_AUTH0_DOMAIN": "dev-6qbeg0e4.auth0.com",
+	}
+
+	// Handle the root route and the login routes:
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", dataToUIPage)
+	})
+
+	apiPort := GetPort()
+	api := "Handling REST-API calls on " + ":" + apiPort
+	fmt.Println(api)
+	router.Run(":" + apiPort)
 }
